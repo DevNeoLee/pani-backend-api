386b9097481b061df447d0f9be163bec58a702ca
DevNeoLee <devneolee@gmail.com>
2021-03-15 13:15:57 -0700

    Setup Categories controller namespace for api, v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do 
+    namespace :v1 do 
+      resources :categories, defaults: { format: json}
+    end
+  end
+
   resources :products
-  resources :categories
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
