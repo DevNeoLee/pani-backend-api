35abb43d9edf2362502fde52767ca8c7bbb60302
DevNeoLee <devneolee@gmail.com>
2021-03-15 15:54:13 -0700

    Make correction products table name on migration

--- db/migrate/20210315205815_add_column_price_to_product.rb
+++ db/migrate/20210315205815_add_column_price_to_product.rb
@@ -1,5 +1,5 @@
 class AddColumnPriceToProduct < ActiveRecord::Migration[6.0]
   def change
-    add_column :Products, :price, :float, precision: 10, scale: 2, null: false, default: 0
+    add_column :products, :price, :float, precision: 10, scale: 2, null: false, default: 0
   end
 end
