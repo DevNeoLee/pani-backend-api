36f717719467b356fb7048263ac5cf90432b0c90
DevNeoLee <devneolee@gmail.com>
2021-03-15 13:19:36 -0700

    Fix type error for json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
-      resources :categories, defaults: { format: json}
+      resources :categories, defaults: { format: :json}
     end
   end
 
