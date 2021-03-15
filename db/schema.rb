0b2b8e7b138666c52082d9843f338c0f56b3dc20
DevNeoLee <devneolee@gmail.com>
2021-03-15 15:34:13 -0700

    Switch sqlite3 to pg database for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_15_205815) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "parent_id"
